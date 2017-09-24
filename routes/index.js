8ccde571c17210d0ae946830f2aecc60f9a0692e
Mike Ekert <mikeekert@gmail.com>
2017-09-24 17:32:58 -0500

    heroku

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,15 @@
 var router = require('express').Router();
 var path = require('path');
 
+// If we are running on Heroku, use the remote database (with SSL)
+if(process.env.DATABASE_URL != undefined) {
+    connectionString = process.env.DATABASE_URL + "?ssl=true";
+} else {
+    // running locally, use our local database instead
+    connectionString = 'postgres://localhost:5432/imagegallery';
+}
+
+
 router.get('/', function(req, res){
     res.sendFile(path.resolve('./public/views/index.html'));
 });
